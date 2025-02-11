87b9a19b3b4baa64c2ff644a9221c4c3deacce27
TetoSolis <164871660+TetoSolis@users.noreply.github.com>
2025-02-11 13:48:26 +0100

    Create DS18B20.ino

---
+++ ESP/DS18B20/DS18B20.ino
@@ -0,0 +1,28 @@
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+#define ONE_WIRE_BUS 21  // GPIO21 (D21)
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+
+void setup() {
+    Serial.begin(115200);
+    while (!Serial);
+    Serial.println("Lecture de la température DS18B20...");
+    sensors.begin();
+}
+
+void loop() {
+    sensors.requestTemperatures();  
+    float temp = sensors.getTempCByIndex(0);
+
+    if (temp == -127.0) {
+        Serial.println("Erreur : Capteur non détecté !");
+    } else {
+        Serial.print("Température : ");
+        Serial.print(temp);
+        Serial.println(" °C");
+    }
+    delay(1000);
+}
